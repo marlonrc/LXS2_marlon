1d1d466f3b9f477d7f532651e6cf51a7506a954e
Marlon Rojas C <marocu.11902@gmail.com>
2016-10-08 11:45:38 -0600

    Inicio SpecVar.sh

---
+++ src/SpecVar.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+## Primero revisamos que el numero de parametros sea el correcto
+#--------------------------------------------------
+# Revision de argumentos
+# Son tres argumentos
+ARGS=3
+
+if [ $# -ne $ARGS ]
+then
+	echo "Error:"
+	echo "Uso: $0 <op1> <op2> <op3>"
+	exit 1
+fi
+
+#--------------------------------------------------
+
+# Quiero ver cuales fueron los argumentos
+
