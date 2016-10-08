db76216e8d57ebda86d8583dd3de4ef525497373
Marlon Rojas C <marocu.11902@gmail.com>
2016-10-08 11:44:25 -0600

    Se modifican el condicional if

--- src/parametros_posicionales.sh
+++ src/parametros_posicionales.sh
@@ -7,6 +7,14 @@ ERRORLOG="$0.err"
 if cp archivo1.txt archivo1.txt.bck 2> $ERRORLOG
 then
 	echo "El archivo1.txt fue correctamente respaldado"
+	echo "Eliminando archivo original"
+
+	if rm archivo1.txt
+	then
+		echo "El archivo original fue eliminado correctamente"
+	else
+		echo "No se pudo eliminar el archivo, revisar $ERRORLOG"
+	fi
 else
 	echo "El archivo1.txt no pudo ser respaldado, verifique el log :$ERRORLOG"
 fi
